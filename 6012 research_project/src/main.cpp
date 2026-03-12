383d1839a4debff8f9af63835f6b3a585cb82f4a
SimonSweatman <simonsweatman@gmail.com>
2026-03-12 10:58:24 +0000

    Initial set up

--- 6012 research_project/src/main.cpp
+++ 6012 research_project/src/main.cpp
@@ -1,18 +1,26 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include "PCA9685.h"
 
-// put function declarations here:
-int myFunction(int, int);
+ServoDriver pwm;
+
+const uint8_t ADDR = 0x70;
+
+// Try 1 first (Seeed examples often use 1..16). If nothing moves, change to 0.
+uint8_t SERVO_CH = 1;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin();
+  Serial.begin(115200);
+
+  pwm.init(ADDR);
+
+  // If your servo range is off / buzzing, uncomment and tune:
+  // pwm.setServoPulseRange(600, 2400, 180);
+
+  Serial.println("Starting single-servo test...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  pwm.setAngle(SERVO_CH, 70);  delay(800);
+  pwm.setAngle(SERVO_CH, 110); delay(800);
 }
